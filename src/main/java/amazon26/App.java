625735fc6acf2297152339b8e71fc940b798d632
swathiRamidi <swathislokam23@gmail.com>
2022-03-22 19:51:17 +0530

    ac service devloped

---
+++ src/main/java/amazon26/App.java
@@ -0,0 +1,13 @@
+package amazon26;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
